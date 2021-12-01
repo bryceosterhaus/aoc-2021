e2b1b0b3e547f553eb92d714d38d4c85881e27c8
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2021-12-01 10:49:32 -0800

    Day 1, Part 2

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn text_input_to_vec(path: &str) -> Vec<u32> {
+fn get_input_file(path: &str) -> Vec<u32> {
 	let contents = fs::read_to_string(path).expect("Unable to read input file");
 
 	let input: Vec<u32> = contents
@@ -23,10 +23,32 @@ fn part_1(input: Vec<u32>) -> u32 {
 	return increases;
 }
 
+fn part_2(input: Vec<u32>) -> u32 {
+	let mut increases = 0;
+	let mut previous_window: u32 = input.windows(3).next().unwrap().iter().sum();
+
+	for window in input.windows(3) {
+		let current_window = window.iter().sum();
+
+		if current_window > previous_window {
+			increases = increases + 1;
+		}
+
+		previous_window = current_window;
+	}
+
+	return increases;
+}
+
 pub fn main() {
 	println!(
 		"Increments: {}",
-		part_1(text_input_to_vec("inputs/day_01/part_1.txt"))
+		part_1(get_input_file("inputs/day_01/input.txt"))
+	);
+
+	println!(
+		"Increments: {}",
+		part_2(get_input_file("inputs/day_01/input.txt"))
 	);
 }
 
@@ -36,6 +58,11 @@ mod tests {
 
 	#[test]
 	fn test_part_1() {
-		assert_eq!(part_1(text_input_to_vec("inputs/day_01/part_1.txt")), 1521);
+		assert_eq!(part_1(get_input_file("inputs/day_01/input.txt")), 1521);
+	}
+
+	#[test]
+	fn test_part_2() {
+		assert_eq!(part_1(get_input_file("inputs/day_01/input.txt")), 1543);
 	}
 }
